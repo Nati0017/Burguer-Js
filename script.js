8913289f2f22dd789220642c2d27ddbb7763b270
Natasha Pérez <165829944+Nati0017@users.noreply.github.com>
2024-06-20 17:28:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Make
+function mostrarAlerta() { 
+    console.log("¡Alguien hizo click en BURGER TOWN!"); 
+    alert("ES HORA DE HAMBURGUESA")
+}
+
+function cambiarColor() {
+    const pictureTitle = document.querySelector("h2"); 
+    pictureTitle.style.color = "red" ; 
+}
